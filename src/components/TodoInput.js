a112443852848ba9964bfe4335c9118a0efd51bf
ebdem <ebu.bekir.3333@gmail.com>
2020-10-18 18:15:28 +0300

    created view components

---
+++ src/components/TodoInput.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TodoInput() {
+    return (
+        <div>
+            <div className='row m-2'>
+                <input type='text'
+                 className=' col form-control'
+                 />
+                <button
+                 className='btn btn-primary mx-2' 
+                 >Add</button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoInput
---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+function TodoList() {
+    return (
+        <div className='my-5'>
+            
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+        </div>
+    )
+}
+
+export default TodoList
+
