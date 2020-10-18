e60888b7abb9fafc3eac2064733ff5d790c80754
ebdem <ebu.bekir.3333@gmail.com>
2020-10-18 22:14:17 +0300

    complete project with Crud proccess

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import TodoItem from './TodoItem'
-
+import React from 'react';
+import TodoItem from './TodoItem';
+import { useSelector } from 'react-redux';
+ 
 function TodoList() {
+
+    let todos  = useSelector(state => state)
     return (
         <div className='my-5'>
             
-            <TodoItem/>
-            <TodoItem/>
-            <TodoItem/>
-            <TodoItem/>
-            <TodoItem/>
+            {
+                todos.map(todo => {
+                   return <TodoItem key={todo.id} todo={todo}/>
+                })
+            }
+            
         </div>
     )
 }
